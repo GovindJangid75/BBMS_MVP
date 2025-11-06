c597c75d652e59403025c425cd2d48675960172d
Govind Jangid <govindjangidt@gmail.com>
2025-11-06 18:25:43 +0530

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL || 'https://blood-bank-management-system-qxy1.onrender.com/api',
+  baseURL: process.env.REACT_APP_BASEURL || 'https://blood-bank-management-system-qxy1.onrender.com',
 });
 
 // Ensure all API routes are directed to the deployed backend
